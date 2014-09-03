431d5e0fd7154f44410b488f8d6a9487a8c51344
Michael Babker <mbabker@flbab.com>
2014-09-03 14:01:06 -0400

    Add MySQL schema updates from 2.5 to 3.4

--- src/com_weblinks/admin/sql/updates/mysql/3.4.0.sql
+++ src/com_weblinks/admin/sql/updates/mysql/3.4.0.sql
@@ -1,1 +1,10 @@
-# Placeholder file to set the extension database schema version
+# This is a rollup of all database schema changes applied from 3.0.0 to 3.3.x
+
+ALTER TABLE `#__weblinks` DROP COLUMN `sid`;
+ALTER TABLE `#__weblinks` DROP COLUMN `date`;
+ALTER TABLE `#__weblinks` DROP COLUMN `archived`;
+ALTER TABLE `#__weblinks` DROP COLUMN `approved`;
+ALTER TABLE `#__weblinks` ENGINE=InnoDB;
+
+ALTER TABLE `#__weblinks` ADD COLUMN `version` int(10) unsigned NOT NULL DEFAULT '1';
+ALTER TABLE `#__weblinks` ADD COLUMN `images` text NOT NULL;
