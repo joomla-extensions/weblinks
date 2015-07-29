0910b638a612adb7057292f38ac3e814d9fad4e0
javier gomez <javier.gomez.corio@gmail.com>
2015-07-29 18:21:47 +0200

    Update Selenium

--- RoboFile.php
+++ RoboFile.php
@@ -147,7 +147,7 @@ class RoboFile extends \Robo\Tasks
 			{
 				$this->say('Downloading Selenium Server, this may take a while.');
 				$this->taskExec('wget')
-				     ->arg('http://selenium-release.storage.googleapis.com/2.45/selenium-server-standalone-2.45.0.jar')
+				     ->arg('http://selenium-release.storage.googleapis.com/2.46/selenium-server-standalone-2.46.0.jar')
 				     ->arg('-O selenium-server-standalone.jar')
 				     ->printed(false)
 				     ->run();
