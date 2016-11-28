3cf373b43cc38e0d2a15bbfb35faab6366ffb845
chrisdavenport <chris.davenport@joomla.org>
2016-11-28 22:13:39 +0000

    Release 3.6.0-beta

--- src/components/com_weblinks/views/categories/view.html.php
+++ src/components/com_weblinks/views/categories/view.html.php
@@ -1,9 +1,8 @@
 <?php
 /**
- * @package     Joomla.Site
- * @subpackage  com_weblinks
+ * @package     Weblinks
  *
- * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
--- src/plugins/finder/weblinks/weblinks.php
+++ src/plugins/finder/weblinks/weblinks.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * @package     Joomla.Plugin
- * @subpackage  Finder.Weblinks
+ * @package     Weblinks
  *
- * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('JPATH_BASE') or die;
