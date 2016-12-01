bb4b0bfb463130ac08c62e53c981d7a9ce60511d
Chris Davenport <chris.davenport@davenporttechnology.com>
2016-12-01 07:21:47 +0000

    Fix for failing code style tests (#283)
    * Fix for failing code style tests
    
    * Changed spacing again and ran the script this time

--- src/plugins/search/weblinks/weblinks.php
+++ src/plugins/search/weblinks/weblinks.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * @package     Weblinks
+ * @package     Joomla.Administrator
+ * @subpackage  Weblinks
  *
  * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
