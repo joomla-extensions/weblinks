85577244a3bdafca937c0a2b382708a7cbf31aee
astridx <info@astrid-guenther.de>
2017-11-04 17:04:47 +0100

    addchangetosecondimage

--- src/components/com_weblinks/views/category/tmpl/default_items.php
+++ src/components/com_weblinks/views/category/tmpl/default_items.php
@@ -143,7 +143,7 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 						<?php endif; ?>
 						<?php  if (isset($images->image_second) and !empty($images->image_second)) : ?>
 						<?php $imgfloat = (empty($images->float_second)) ? $this->params->get('float_second') : $images->float_second; ?>
-						<div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image"> <img
+						<div class="pull-<?php echo htmlspecialchars($imgfloat, ENT_COMPAT, 'UTF-8'); ?> item-image"> <img
 						<?php if ($images->image_second_caption) : ?>
 							<?php echo 'class="caption" title="' . htmlspecialchars($images->image_second_caption) . '"'; ?>
 						<?php endif; ?>
