84df5980155320a76a2771a5fe327d06d50bc107
Astrid <astridx@users.noreply.github.com>
2017-11-06 12:56:26 +0100

    makeSameAliasPossibleForDifferentLanguages (#373)

--- src/administrator/components/com_weblinks/tables/weblink.php
+++ src/administrator/components/com_weblinks/tables/weblink.php
@@ -93,7 +93,7 @@ class WeblinksTableWeblink extends JTable
 		// Verify that the alias is unique
 		$table = JTable::getInstance('Weblink', 'WeblinksTable');
 
-		if ($table->load(array('alias' => $this->alias, 'catid' => $this->catid)) && ($table->id != $this->id || $this->id == 0))
+		if ($table->load(array('language' => $this->language, 'alias' => $this->alias, 'catid' => $this->catid)) && ($table->id != $this->id || $this->id == 0))
 		{
 			$this->setError(JText::_('COM_WEBLINKS_ERROR_UNIQUE_ALIAS'));
 
@@ -136,6 +136,7 @@ class WeblinksTableWeblink extends JTable
 			->select($db->quoteName('id'))
 			->from($db->quoteName('#__weblinks'))
 			->where($db->quoteName('title') . ' = ' . $db->quote($this->title))
+			->where($db->quoteName('language') . ' = ' . $db->quote($this->language))
 			->where($db->quoteName('catid') . ' = ' . (int) $this->catid);
 		$db->setQuery($query);
 
