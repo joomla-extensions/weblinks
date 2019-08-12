0716d5e3e624071ff5da1619d15d8f6318e8c42c
wilsonge <georgejameswilson@googlemail.com>
2019-08-13 00:15:35 +0100

    Fix code style errors in project

--- src/administrator/components/com_weblinks/tables/weblink.php
+++ src/administrator/components/com_weblinks/tables/weblink.php
@@ -93,7 +93,8 @@ class WeblinksTableWeblink extends JTable
 		// Verify that the alias is unique
 		$table = JTable::getInstance('Weblink', 'WeblinksTable', array('dbo' => $this->getDbo()));
 
-		if ($table->load(array('language' => $this->language, 'alias' => $this->alias, 'catid' => $this->catid)) && ($table->id != $this->id || $this->id == 0))
+		if ($table->load(array('language' => $this->language, 'alias' => $this->alias, 'catid' => $this->catid))
+			&& ($table->id != $this->id || $this->id == 0))
 		{
 			$this->setError(JText::_('COM_WEBLINKS_ERROR_UNIQUE_ALIAS'));
 
