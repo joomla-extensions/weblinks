7406eca92f8ce88eef650c21ca57c668d57325dc
George Wilson <georgejameswilson@googlemail.com>
2019-08-16 14:52:11 +0100

    Fix modal call

--- src/administrator/components/com_weblinks/views/weblinks/view.html.php
+++ src/administrator/components/com_weblinks/views/weblinks/view.html.php
@@ -103,6 +103,7 @@ class WeblinksViewWeblinks extends JViewLegacy
 		if ($user->authorise('core.create', 'com_weblinks') && $user->authorise('core.edit', 'com_weblinks')
 			&& $user->authorise('core.edit.state', 'com_weblinks'))
 		{
+			JHtml::_('bootstrap.renderModal', 'collapseModal');
 			$title = JText::_('JTOOLBAR_BATCH');
 
 			// Instantiate a new JLayoutFile instance and render the batch button
