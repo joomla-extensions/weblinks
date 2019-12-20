12e94528bd45b5ee51a153f29bf5ec97c7f0327d
Richard Fath <richard67@users.noreply.github.com>
2019-12-20 15:21:29 +0100

    Null datetimes and default values - SQL changes

---
+++ src/administrator/components/com_weblinks/sql/updates/mysql/4.0.0.sql
@@ -0,0 +1,31 @@
+ALTER TABLE `#__weblinks` MODIFY `created` datetime NOT NULL;
+ALTER TABLE `#__weblinks` MODIFY `modified` datetime NOT NULL;
+
+ALTER TABLE `#__weblinks` MODIFY `publish_up` datetime NULL DEFAULT NULL;
+ALTER TABLE `#__weblinks` MODIFY `publish_down` datetime NULL DEFAULT NULL;
+ALTER TABLE `#__weblinks` MODIFY `checked_out_time` datetime NULL DEFAULT NULL;
+
+UPDATE `#__weblinks` SET `modified` = `created` WHERE `modified` = '0000-00-00 00:00:00';
+
+UPDATE `#__weblinks` SET `publish_up` = NULL WHERE `publish_up` = '0000-00-00 00:00:00';
+UPDATE `#__weblinks` SET `publish_down` = NULL WHERE `publish_down` = '0000-00-00 00:00:00';
+UPDATE `#__weblinks` SET `checked_out_time` = NULL WHERE `checked_out_time` = '0000-00-00 00:00:00';
+
+UPDATE `#__categories` SET `modified_time` = `created_time` WHERE `modified_time` = '0000-00-00 00:00:00' AND `extension` = 'com_weblinks';
+
+UPDATE `#__categories` SET `checked_out_time` = NULL WHERE `checked_out_time` = '0000-00-00 00:00:00' AND `extension` = 'com_weblinks';
+
+UPDATE `#__ucm_content` SET `core_modified_time` = `core_created_time`
+ WHERE `core_type_alias` IN ('com_weblinks.weblink', 'com_weblinks.category')
+   AND `core_modified_time` = '0000-00-00 00:00:00';
+
+UPDATE `#__ucm_content` SET `core_publish_up` = NULL
+ WHERE `core_type_alias` = 'com_weblinks.weblink'
+   AND `core_publish_up` = '0000-00-00 00:00:00';
+UPDATE `#__ucm_content` SET `core_publish_down` = NULL
+ WHERE `core_type_alias` = 'com_weblinks.weblink'
+   AND `core_publish_down` = '0000-00-00 00:00:00';
+
+UPDATE `#__ucm_content` SET `core_checked_out_time` = NULL
+ WHERE `core_type_alias` IN ('com_weblinks.weblink', 'com_weblinks.category')
+   AND `core_checked_out_time` = '0000-00-00 00:00:00';
---
+++ src/administrator/components/com_weblinks/sql/updates/postgresql/4.0.0.sql
@@ -0,0 +1,37 @@
+ALTER TABLE "#__weblinks" ALTER COLUMN "created" DROP DEFAULT;
+
+ALTER TABLE "#__weblinks" ALTER COLUMN "modified" DROP DEFAULT;
+
+ALTER TABLE "#__weblinks" ALTER COLUMN "publish_up" DROP NOT NULL;
+ALTER TABLE "#__weblinks" ALTER COLUMN "publish_up" DROP DEFAULT;
+
+ALTER TABLE "#__weblinks" ALTER COLUMN "publish_down" DROP NOT NULL;
+ALTER TABLE "#__weblinks" ALTER COLUMN "publish_down" DROP DEFAULT;
+
+ALTER TABLE "#__weblinks" ALTER COLUMN "checked_out_time" DROP NOT NULL;
+ALTER TABLE "#__weblinks" ALTER COLUMN "checked_out_time" DROP DEFAULT;
+
+UPDATE "#__weblinks" SET "modified" = "created" WHERE "modified" = '1970-01-01 00:00:00';
+
+UPDATE "#__weblinks" SET "publish_up" = NULL WHERE "publish_up" = '1970-01-01 00:00:00';
+UPDATE "#__weblinks" SET "publish_down" = NULL WHERE "publish_down" = '1970-01-01 00:00:00';
+UPDATE "#__weblinks" SET "checked_out_time" = NULL WHERE "checked_out_time" = '1970-01-01 00:00:00';
+
+UPDATE "#__categories" SET "modified_time" = "created_time" WHERE "modified_time" = '1970-01-01 00:00:00' AND "extension" = 'com_weblinks';
+
+UPDATE "#__categories" SET "checked_out_time" = NULL WHERE "checked_out_time" = '1970-01-01 00:00:00' AND "extension" = 'com_weblinks';
+
+UPDATE "#__ucm_content" SET "core_modified_time" = "core_created_time"
+ WHERE "core_type_alias" IN ('com_weblinks.weblink', 'com_weblinks.category')
+   AND "core_modified_time" = '1970-01-01 00:00:00';
+
+UPDATE "#__ucm_content" SET "core_publish_up" = NULL
+ WHERE "core_type_alias" = 'com_weblinks.weblink'
+   AND "core_publish_up" = '1970-01-01 00:00:00';
+UPDATE "#__ucm_content" SET "core_publish_down" = NULL
+ WHERE "core_type_alias" = 'com_weblinks.weblink'
+   AND "core_publish_down" = '1970-01-01 00:00:00';
+
+UPDATE "#__ucm_content" SET "core_checked_out_time" = NULL
+ WHERE "core_type_alias" IN ('com_weblinks.weblink', 'com_weblinks.category')
+   AND "core_checked_out_time" = '1970-01-01 00:00:00';
