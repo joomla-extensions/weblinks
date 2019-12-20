afb742ae2e085cffcde8110665de90e30614ee96
Richard Fath <richard67@users.noreply.github.com>
2019-12-20 15:55:10 +0100

    Don't update category data in update sql

--- src/administrator/components/com_weblinks/sql/updates/mysql/4.0.0.sql
+++ src/administrator/components/com_weblinks/sql/updates/mysql/4.0.0.sql
@@ -11,10 +11,6 @@ UPDATE `#__weblinks` SET `publish_up` = NULL WHERE `publish_up` = '0000-00-00 00
 UPDATE `#__weblinks` SET `publish_down` = NULL WHERE `publish_down` = '0000-00-00 00:00:00';
 UPDATE `#__weblinks` SET `checked_out_time` = NULL WHERE `checked_out_time` = '0000-00-00 00:00:00';
 
-UPDATE `#__categories` SET `modified_time` = `created_time` WHERE `modified_time` = '0000-00-00 00:00:00' AND `extension` = 'com_weblinks';
-
-UPDATE `#__categories` SET `checked_out_time` = NULL WHERE `checked_out_time` = '0000-00-00 00:00:00' AND `extension` = 'com_weblinks';
-
 UPDATE `#__ucm_content` SET `core_modified_time` = `core_created_time`
  WHERE `core_type_alias` IN ('com_weblinks.weblink', 'com_weblinks.category')
    AND `core_modified_time` = '0000-00-00 00:00:00';
