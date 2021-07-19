85dfec4235b6822297cf8372a19f9259b599e913
Harald Leithner <leithner@itronic.at>
2021-07-19 15:07:41 +0200

    Update build.sh

--- .drone/build.sh
+++ .drone/build.sh
@@ -16,9 +16,6 @@ composer install --no-interaction --no-progress
 
 mkdir -p /tests/www
 cp -r ./* /tests/www
-mkdir _output
-chmod +rwx _output
-ln -s _output /tests/www/tests/_output
 
 cd /tests/www
 
