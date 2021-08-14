a25ea580fb79faa75ef767765502dc10a9677113
Tuan Pham Ngoc <github@joomdonation.com>
2021-08-14 17:40:26 +0700

    Remove access check because it is handled inside modal already

--- src/components/com_weblinks/tmpl/category/default_items.php
+++ src/components/com_weblinks/tmpl/category/default_items.php
@@ -70,134 +70,125 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 		<?php else : ?>
 			<ul class="category list-unstyled">
 				<?php foreach ($this->items as $i => $item) : ?>
+                    <?php
+                    // Shouldn't this be only for users with admin rights?
+                    // @ToDo: what is the difference -class system-unbublished?
+                    if ($item->state == 0) : ?>
+                        <li class="system-unpublished list-group mt-3">
+                    <?php else : ?>
+                        <li class="list-group mt-3">
+                    <?php endif; ?>
 
-					<?php // Shouldn't this be checked in the model?The pagination will be affected
-					if (in_array($item->access, $this->user->getAuthorisedViewLevels())) : ?>
+                    <?php if ($canEdit) : ?>
+                        <div class="icons list-group-item">
+                            <?php echo HTMLHelper::_('weblinkicon.edit', $item, $this->params); ?>
+                        </div>
+                    <?php endif; ?>
 
-						<?php
-						// Shouldn't this be only for users with admin rights?
-						// @ToDo: what is the difference -class system-unbublished?
-						if ($item->state == 0) : ?>
-							<li class="system-unpublished list-group mt-3">
-						<?php else : ?>
-							<li class="list-group mt-3">
-						<?php endif; ?>
+                    <div class="list-title list-group-item ">
+                        <?php if (!$this->params->get('icons', 1)) : ?>
+                            <?php echo Text::_('COM_WEBLINKS_LINK'); ?>
+                        <?php else : ?>
+                            <?php // ToDo css icons as variables ?>
+                            <?php if (!$this->params->get('link_icons')) : ?>
+                                <span class="icon-globe" aria-hidden="true"></span>
+                            <?php else: ?>
+                                <?php echo '<img src="' . $this->params->get('link_icons') . '" alt="' . Text::_('COM_WEBLINKS_LINK') . '" />'; ?>
+                            <?php endif; ?>
+                        <?php endif; ?>
 
-						<?php if ($canEdit) : ?>
-							<div class="icons list-group-item">
-								<?php echo HTMLHelper::_('weblinkicon.edit', $item, $this->params); ?>
-							</div>
-						<?php endif; ?>
+                        <?php // Compute the correct link ?>
+                        <?php $menuclass = 'category' . $this->pageclass_sfx; ?>
+                        <?php $link   = $item->link; ?>
+                        <?php $width  = $item->params->get('width', 600); ?>
+                        <?php $height = $item->params->get('height', 500); ?>
+                        
+                        <?php if ($item->state == 0) : ?>
+                            <span class="badge bg-warning"><?php echo Text::_('JUNPUBLISHED'); ?></span>
+                        <?php endif; ?>
 
-						<div class="list-title list-group-item ">
-							<?php if (!$this->params->get('icons', 1)) : ?>
-								<?php echo Text::_('COM_WEBLINKS_LINK'); ?>
-							<?php else : ?>
-								<?php // ToDo css icons as variables ?>
-								<?php if (!$this->params->get('link_icons')) : ?>
-									<span class="icon-globe" aria-hidden="true"></span>
-								<?php else: ?>
-									<?php echo '<img src="' . $this->params->get('link_icons') . '" alt="' . Text::_('COM_WEBLINKS_LINK') . '" />'; ?>
-								<?php endif; ?>
-							<?php endif; ?>
+                        <?php
+                        switch ($item->params->get('target', $this->params->get('target')))
+                        {
+                            case 1:
+                                // Open in a new window
+                                echo '<a href="' . $link . '" target="_blank" class="' . $menuclass . '" rel="nofollow">' .
+                                    $this->escape($item->title) . '</a>';
+                                break;
 
-							<?php // Compute the correct link ?>
-							<?php $menuclass = 'category' . $this->pageclass_sfx; ?>
-							<?php $link   = $item->link; ?>
-							<?php $width  = $item->params->get('width'); ?>
-							<?php $height = $item->params->get('height'); ?>
-							<?php if ($width == null || $height == null) : ?>
-								<?php $width  = 600; ?>
-								<?php $height = 500; ?>
-							<?php endif; ?>
+                            case 2:
+                                // Open in a popup window
+                                $attribs = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=' . $this->escape($width) . ',height=' . $this->escape($height) . '';
+                                echo "<a href=\"$link\" onclick=\"window.open(this.href, 'targetWindow', '" . $attribs . "'); return false;\">" .
+                                    $this->escape($item->title) . '</a>';
+                                break;
+                            case 3:
+                                // Open in a modal window
+                                HTMLHelper::_('behavior.modal', 'a.modal');
+                                echo '<a class="modal" href="' . $link . '"  rel="{handler: \'iframe\', size: {x:' . $this->escape($width) . ', y:' . $this->escape($height) . '}}">' .
+                                    $this->escape($item->title) . ' </a>';
+                                break;
 
-							<?php if ($item->state == 0) : ?>
-								<span class="badge bg-warning"><?php echo Text::_('JUNPUBLISHED'); ?></span>
-							<?php endif; ?>
+                            default:
+                                // Open in parent window
+                                echo '<a href="' . $link . '" class="' . $menuclass . '" rel="nofollow">' .
+                                    $this->escape($item->title) . ' </a>';
+                                break;
+                            }
+                            ?>
+                            <?php if ($this->params->get('show_link_hits', 1)) : ?>
+                                <div class="list-hits badge bg-info float-end">
+                                    <?php echo Text::sprintf('JGLOBAL_HITS_COUNT', $item->hits); ?>
+                                </div>
+                            <?php endif; ?>
 
-							<?php
-							switch ($item->params->get('target', $this->params->get('target')))
-							{
-								case 1:
-									// Open in a new window
-									echo '<a href="' . $link . '" target="_blank" class="' . $menuclass . '" rel="nofollow">' .
-										$this->escape($item->title) . '</a>';
-									break;
+                            <?php if ($this->params->get('show_tags', 1) && !empty($item->tags->itemTags)) : ?>
+                                <div class="mt-2 mb-2">
+                                    <?php echo LayoutHelper::render('joomla.content.tags', $item->tags->itemTags); ?>
+                                </div>
+                            <?php endif; ?>
 
-								case 2:
-									// Open in a popup window
-									$attribs = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=' . $this->escape($width) . ',height=' . $this->escape($height) . '';
-									echo "<a href=\"$link\" onclick=\"window.open(this.href, 'targetWindow', '" . $attribs . "'); return false;\">" .
-										$this->escape($item->title) . '</a>';
-									break;
-								case 3:
-									// Open in a modal window
-									HTMLHelper::_('behavior.modal', 'a.modal');
-									echo '<a class="modal" href="' . $link . '"  rel="{handler: \'iframe\', size: {x:' . $this->escape($width) . ', y:' . $this->escape($height) . '}}">' .
-										$this->escape($item->title) . ' </a>';
-									break;
+                            <?php if (($this->params->get('show_link_description')) && ($item->description != '')) : ?>
+                                <div class="mt-2 mb-2">
+                                    <?php $images = json_decode($item->images); ?>
+                                    <?php  if (!empty($images->image_first)) : ?>
+                                        <?php $imgfloat = (empty($images->float_first)) ? $this->params->get('float_first') : $images->float_first; ?>
+                                        <?php $img      = HTMLHelper::cleanImageURL($images->image_first); ?>
+                                        <?php $alt      = empty($images->image_first_alt) && empty($images->image_first_alt_empty)
+                                            ? ''
+                                            : 'alt="' . htmlspecialchars($images->image_first_alt, ENT_COMPAT, 'UTF-8') . '"'; ?>
+                                        <figure class="item-image">
+                                            <img src="<?php echo htmlspecialchars($img->url, ENT_COMPAT, 'UTF-8'); ?>"
+                                                    <?php echo $alt; ?> itemprop="thumbnail" />
+                                            <?php if (!empty($images->image_first_caption)) : ?>
+                                                <figcaption class="caption"><?php echo htmlspecialchars($images->image_first_caption, ENT_COMPAT, 'UTF-8'); ?></figcaption>
+                                            <?php endif; ?>
+                                        </figure>
+                                    <?php endif; ?>
 
-								default:
-									// Open in parent window
-									echo '<a href="' . $link . '" class="' . $menuclass . '" rel="nofollow">' .
-										$this->escape($item->title) . ' </a>';
-									break;
-								}
-								?>
-								<?php if ($this->params->get('show_link_hits', 1)) : ?>
-									<div class="list-hits badge bg-info float-end">
-										<?php echo Text::sprintf('JGLOBAL_HITS_COUNT', $item->hits); ?>
-									</div>
-								<?php endif; ?>
+                                    <?php  if (!empty($images->image_second)) : ?>
+                                        <?php $imgfloat = (empty($images->float_second)) ? $this->params->get('float_second') : $images->float_second; ?>
+                                        <?php $img      = HTMLHelper::cleanImageURL($images->image_second); ?>
+                                        <?php $alt      = empty($images->image_second_alt) && empty($images->image_second_alt_empty)
+                                            ? ''
+                                            : 'alt="' . htmlspecialchars($images->image_second_alt, ENT_COMPAT, 'UTF-8') . '"'; ?>
+                                        <figure class="item-image">
+                                            <img src="<?php echo htmlspecialchars($img->url, ENT_COMPAT, 'UTF-8'); ?>"
+                                                    <?php echo $alt; ?> itemprop="thumbnail" />
+                                            <?php if (!empty($images->image_second_caption)) : ?>
+                                                <figcaption class="caption"><?php echo htmlspecialchars($images->image_second_caption, ENT_COMPAT, 'UTF-8'); ?></figcaption>
+                                            <?php endif; ?>
+                                        </figure>
+                                    <?php endif; ?>
 
-								<?php if ($this->params->get('show_tags', 1) && !empty($item->tags->itemTags)) : ?>
-									<div class="mt-2 mb-2">
-										<?php echo LayoutHelper::render('joomla.content.tags', $item->tags->itemTags); ?>
-									</div>
-								<?php endif; ?>
+                                    <?php echo $item->description; ?>
 
-								<?php if (($this->params->get('show_link_description')) && ($item->description != '')) : ?>
-									<div class="mt-2 mb-2">
-										<?php $images = json_decode($item->images); ?>
-										<?php  if (!empty($images->image_first)) : ?>
-											<?php $imgfloat = (empty($images->float_first)) ? $this->params->get('float_first') : $images->float_first; ?>
-											<?php $img      = HTMLHelper::cleanImageURL($images->image_first); ?>
-											<?php $alt      = empty($images->image_first_alt) && empty($images->image_first_alt_empty)
-												? ''
-												: 'alt="' . htmlspecialchars($images->image_first_alt, ENT_COMPAT, 'UTF-8') . '"'; ?>
-											<figure class="item-image">
-												<img src="<?php echo htmlspecialchars($img->url, ENT_COMPAT, 'UTF-8'); ?>"
-														<?php echo $alt; ?> itemprop="thumbnail" />
-												<?php if (!empty($images->image_first_caption)) : ?>
-													<figcaption class="caption"><?php echo htmlspecialchars($images->image_first_caption, ENT_COMPAT, 'UTF-8'); ?></figcaption>
-												<?php endif; ?>
-											</figure>
-										<?php endif; ?>
+                                </div>
+                            <?php endif; ?>
 
-										<?php  if (!empty($images->image_second)) : ?>
-											<?php $imgfloat = (empty($images->float_second)) ? $this->params->get('float_second') : $images->float_second; ?>
-											<?php $img      = HTMLHelper::cleanImageURL($images->image_second); ?>
-											<?php $alt      = empty($images->image_second_alt) && empty($images->image_second_alt_empty)
-												? ''
-												: 'alt="' . htmlspecialchars($images->image_second_alt, ENT_COMPAT, 'UTF-8') . '"'; ?>
-											<figure class="item-image">
-												<img src="<?php echo htmlspecialchars($img->url, ENT_COMPAT, 'UTF-8'); ?>"
-														<?php echo $alt; ?> itemprop="thumbnail" />
-												<?php if (!empty($images->image_second_caption)) : ?>
-													<figcaption class="caption"><?php echo htmlspecialchars($images->image_second_caption, ENT_COMPAT, 'UTF-8'); ?></figcaption>
-												<?php endif; ?>
-											</figure>
-										<?php endif; ?>
+                        </div>
 
-										<?php echo $item->description; ?>
-
-									</div>
-								<?php endif; ?>
-
-							</div>
-
-						</li>
-					<?php endif; ?>
+                    </li>
 				<?php endforeach; ?>
 			</ul>
 
