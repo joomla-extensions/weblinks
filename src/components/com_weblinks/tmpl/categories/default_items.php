95b07343a9b565ce05fc83a177a1aacf3a4911be
chmst <dev@chmst.de>
2021-09-25 23:32:56 +0200

    repeat adapting to com_categories

--- src/components/com_weblinks/tmpl/categories/default_items.php
+++ src/components/com_weblinks/tmpl/categories/default_items.php
@@ -16,18 +16,20 @@ use Joomla\Component\Weblinks\Site\Helper\RouteHelper;
 
 if ($this->maxLevelcat != 0 && count($this->items[$this->parent->id]) > 0) :
 ?>
-	<?php foreach ($this->items[$this->parent->id] as $id => $item) : ?>
-	<?php if ($this->params->get('show_empty_categories_cat') || $item->numitems || count($item->getChildren())) : ?>
-		<div class="com-weblinks-categories__items">
-			<h3 class="page-header item-title">
+	<div class="com-content-categories__items">
+		<?php foreach ($this->items[$this->parent->id] as $id => $item) : ?>
+			<?php if ($this->params->get('show_empty_categories_cat') || $item->numitems || count($item->getChildren())) : ?>
+			<div class="com-content-categories__item">
+				<div>
 				<a href="<?php echo Route::_(RouteHelper::getCategoryRoute($item->id, $item->language)); ?>">
 					<?php echo $this->escape($item->title); ?></a>
-				<?php if ($this->params->get('show_cat_num_links_cat') == 1) :?>
-					<span class="badge bg-info">
-							<?php echo Text::_('COM_WEBLINKS_NUM_ITEMS'); ?>&nbsp;
-							<?php echo $item->numitems; ?>
+					<?php if ($this->params->get('show_cat_num_links_cat') == 1) :?>
+						<span class="badge bg-info">
+								<?php echo Text::_('COM_WEBLINKS_NUM_ITEMS'); ?>&nbsp;
+								<?php echo $item->numitems; ?>
 						</span>
-				<?php endif; ?>
+					<?php endif; ?>
+				</div>
 				<?php if ($this->maxLevelcat > 1 && count($item->getChildren()) > 0) : ?>
 					<button
 							type="button"
@@ -40,28 +42,30 @@ if ($this->maxLevelcat != 0 && count($this->items[$this->parent->id]) > 0) :
 						<span class="icon-plus" aria-hidden="true"></span>
 					</button>
 				<?php endif; ?>
-			</h3>
-			<?php if ($this->params->get('show_subcat_desc_cat') == 1) : ?>
-				<?php if ($item->description) : ?>
-					<div class="category-desc">
-						<?php echo HTMLHelper::_('content.prepare', $item->description, '', 'com_weblinks.categories'); ?>
+				<?php if ($this->params->get('show_subcat_desc_cat') == 1) : ?>
+					<?php if ($item->description) : ?>
+						<div class="category-desc">
+							<?php echo HTMLHelper::_('content.prepare', $item->description, '', 'com_weblinks.categories'); ?>
+						</div>
+					<?php endif; ?>
+				<?php endif; ?>
+				<?php if ($this->params->get('show_description_image') && $item->getParams()->get('image')) : ?>
+					<img src="<?php echo $item->getParams()->get('image'); ?>" alt="<?php echo htmlspecialchars($item->getParams()->get('image_alt'), ENT_COMPAT, 'UTF-8'); ?>">
+				<?php endif; ?>
+				<?php if ($this->maxLevelcat > 1 && count($item->getChildren()) > 0) : ?>
+					<div class="com-content-categories__children" id="category-<?php echo $item->id; ?>" hidden>
+						<?php
+						$this->items[$item->id] = $item->getChildren();
+						$this->parent = $item;
+						$this->maxLevelcat--;
+						echo $this->loadTemplate('items');
+						$this->parent = $item->getParent();
+						$this->maxLevelcat++;
+						?>
 					</div>
 				<?php endif; ?>
+			</div>
 			<?php endif; ?>
-
-			<?php if ($this->maxLevelcat > 1 && count($item->getChildren()) > 0) : ?>
-				<div class="com-weblinks-categories__children" id="category-<?php echo $item->id; ?>" hidden>
-					<?php
-					$this->items[$item->id] = $item->getChildren();
-					$this->parent = $item;
-					$this->maxLevelcat--;
-					echo $this->loadTemplate('items');
-					$this->parent = $item->getParent();
-					$this->maxLevelcat++;
-					?>
-				</div>
-			<?php endif; ?>
-		</div>
-	<?php endif; ?>
-<?php endforeach; ?>
+		<?php endforeach; ?>
+	</div>
 <?php endif; ?>
