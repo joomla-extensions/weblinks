f1ef9662d02b4c2f0732df16569ca9a4ade3ff88
Tuan Pham Ngoc <github@joomdonation.com>
2021-10-02 12:02:33 +0700

    Do not show Empty Trash button for empty state

--- src/administrator/components/com_weblinks/src/View/Weblinks/HtmlView.php
+++ src/administrator/components/com_weblinks/src/View/Weblinks/HtmlView.php
@@ -182,7 +182,7 @@ class HtmlView extends BaseHtmlView
 			}
 		}
 
-		if ($this->state->get('filter.published') == -2 && $canDo->get('core.delete'))
+		if (!$this->isEmptyState && $this->state->get('filter.published') == -2 && $canDo->get('core.delete'))
 		{
 			$toolbar->delete('weblinks.delete')
 				->text('JTOOLBAR_EMPTY_TRASH')
