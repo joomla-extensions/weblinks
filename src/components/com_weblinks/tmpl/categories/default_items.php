e01ce0337d879ff34ba8acbf46f5a2f2669b86cc
chmst <dev@chmst.de>
2021-10-06 13:50:05 +0200

    move expand button to first line of a category

--- src/components/com_weblinks/tmpl/categories/default_items.php
+++ src/components/com_weblinks/tmpl/categories/default_items.php
@@ -20,14 +20,26 @@ if ($this->maxLevelcat != 0 && count($this->items[$this->parent->id]) > 0) :
 		<?php foreach ($this->items[$this->parent->id] as $id => $item) : ?>
 			<?php if ($this->params->get('show_empty_categories_cat') || $item->numitems || count($item->getChildren())) : ?>
 			<div class="com-content-categories__item">
-				<div>
+				<div class="w-100">
 					<a href="<?php echo Route::_(RouteHelper::getCategoryRoute($item->id, $item->language)); ?>">
 						<?php echo $this->escape($item->title); ?></a>
 					<?php if ($this->params->get('show_cat_num_links_cat') == 1) :?>
-						<span class="badge bg-info">
+						<span class="badge bg-info ">
 								<?php echo Text::_('COM_WEBLINKS_NUM_ITEMS'); ?>&nbsp;
 								<?php echo $item->numitems; ?>
 						</span>
+					<?php endif; ?>
+					<?php if ($this->maxLevelcat > 1 && count($item->getChildren()) > 0) : ?>
+						<button
+								type="button"
+								id="category-btn-<?php echo $item->id; ?>"
+								data-category-id="<?php echo $item->id; ?>"
+								class="btn btn-secondary btn-sm float-end"
+								aria-expanded="false"
+								aria-label="<?php echo Text::_('JGLOBAL_EXPAND_CATEGORIES'); ?>"
+						>
+							<span class="icon-plus" aria-hidden="true"></span>
+						</button>
 					<?php endif; ?>
 				</div>
 
@@ -51,18 +63,7 @@ if ($this->maxLevelcat != 0 && count($this->items[$this->parent->id]) > 0) :
 					<img src="<?php echo htmlspecialchars($img->url, ENT_COMPAT, 'UTF-8'); ?>"<?php echo $alt; ?>>
 				<?php endif; ?>
 
-				<?php if ($this->maxLevelcat > 1 && count($item->getChildren()) > 0) : ?>
-					<button
-							type="button"
-							id="category-btn-<?php echo $item->id; ?>"
-							data-category-id="<?php echo $item->id; ?>"
-							class="btn btn-secondary btn-sm float-end"
-							aria-expanded="false"
-							aria-label="<?php echo Text::_('JGLOBAL_EXPAND_CATEGORIES'); ?>"
-					>
-						<span class="icon-plus" aria-hidden="true"></span>
-					</button>
-				<?php endif; ?>
+
 				<?php if ($this->maxLevelcat > 1 && count($item->getChildren()) > 0) : ?>
 					<div class="com-content-categories__children" id="category-<?php echo $item->id; ?>" hidden>
 						<?php
