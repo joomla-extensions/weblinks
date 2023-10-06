43cece800a15da082a50c7f284ca822b150c1587
Hoochicken <mareike.riegel@ql.de>
2023-10-06 17:34:18 +0200

    Update mod_weblinks.php Php 8.1-Fix
    cast string for htmlspecialchars()

--- src/modules/mod_weblinks/mod_weblinks.php
+++ src/modules/mod_weblinks/mod_weblinks.php
@@ -19,6 +19,6 @@ if (!count($list))
 	return;
 }
 
-$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+$moduleclass_sfx = htmlspecialchars((string)$params->get('moduleclass_sfx'));
 
 require JModuleHelper::getLayoutPath('mod_weblinks', $params->get('layout', 'default'));
