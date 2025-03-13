c6898824af5030beab11e0240e0847d1a3fd104f
Nicola Galgano <optimus4joomla@gmail.com>
2025-03-13 10:53:40 +0100

    site weblink test + submit link

--- tests/cypress/integration/site/components/com_weblinks/Weblinks.cy.js
+++ tests/cypress/integration/site/components/com_weblinks/Weblinks.cy.js
@@ -1,5 +1,7 @@
-describe('Test in frontend that the weblinks category view', () => {
-  it('can display a list of weblinks without a menu item', () => {
+describe('Test in frontend that the weblinks', () => {
+  beforeEach(() => cy.task('queryDB', "DELETE FROM #__weblinks WHERE title like '%test weblink%'"));
+
+  it('can display a list of weblinks in a category', () => {
     cy.db_createWeblink({ title: 'automated test weblink 1' })
       .then((weblink) => cy.db_createWeblink({ title: 'automated test weblink 2', catid: weblink.catid }))
       .then((weblink) => cy.db_createWeblink({ title: 'automated test weblink 3', catid: weblink.catid }))
@@ -14,4 +16,32 @@ describe('Test in frontend that the weblinks category view', () => {
       });
   });
 
+  it('can display a single weblink', () => {
+    // Create a single weblink
+    cy.db_createWeblink({ title: 'Single test weblink', url: 'http://example.com' })
+      .then((weblink) => {
+        // Visit the weblink view page
+        cy.visit(`/index.php?option=com_weblinks&view=weblink&id=${weblink.id}`);
+
+        // Check if the weblink is displayed
+        cy.contains('Single test weblink');
+        cy.get('a[href="http://example.com"]').should('exist');
+      });
+  });
+
+  it('can create a weblink through the form', () => {
+    cy.doFrontendLogin();
+    // Visit the weblink creation form
+    cy.visit('/index.php?option=com_weblinks&view=form&layout=edit');
+
+    // Fill out the form
+    cy.get('#jform_title').type('Form test weblink');
+    cy.get('#jform_url').type('http://example.com');
+    // Click the save button
+    cy.get('button.btn.btn-primary[onclick="Joomla.submitbutton(\'weblink.save\')"]').click();
+
+    // Check if the weblink is successfully saved
+    cy.checkForSystemMessage('Web link successfully saved.')
+  });
 });
+
