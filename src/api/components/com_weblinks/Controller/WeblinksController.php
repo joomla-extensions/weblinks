f07ffc783a4938c8cd286a5d95759c91f1e6c702
Nicola Galgano <optimus4joomla@gmail.com>
2025-03-18 09:59:10 +0100

    code style

--- src/api/components/com_weblinks/Controller/WeblinksController.php
+++ src/api/components/com_weblinks/Controller/WeblinksController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package     Joomla.API
  * @subpackage  com_weblinks
@@ -9,19 +10,10 @@
 
 namespace Joomla\Component\Weblinks\Api\Controller;
 
-defined('_JEXEC') or die;
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\Access\Exception\NotAllowed;
-use Joomla\CMS\Application\CMSApplication;
-use Joomla\CMS\Form\Form;
 use Joomla\CMS\MVC\Controller\ApiController;
-use Joomla\CMS\MVC\Factory\LegacyFactory;
-use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
-use Joomla\CMS\MVC\View\JsonApiView;
-use Joomla\CMS\MVC\Controller\Exception\ResourceNotFound;
 use Joomla\Component\Fields\Administrator\Helper\FieldsHelper;
-use Joomla\Input\Input;
-use Tobscure\JsonApi\Exception\InvalidParameterException;
 
 /**
  * The weblinks controller
@@ -30,48 +22,46 @@ use Tobscure\JsonApi\Exception\InvalidParameterException;
  */
 class WeblinksController extends ApiController
 {
-	/**
-	 * The content type of the item.
-	 *
-	 * @var    string
-	 * @since  4.0.0
-	 */
-	protected $contentType = 'weblinks';
+    /**
+     * The content type of the item.
+     *
+     * @var    string
+     * @since  4.0.0
+     */
+    protected $contentType = 'weblinks';
 
-	/**
-	 * The default view for the display method.
-	 *
-	 * @var    string
-	 * @since  3.0
-	 */
-	protected $default_view = 'weblinks';
+    /**
+     * The default view for the display method.
+     *
+     * @var    string
+     * @since  3.0
+     */
+    protected $default_view = 'weblinks';
 
-	/**
-	 * Method to save a record.
-	 *
-	 * @param   integer  $recordKey  The primary key of the item (if exists)
-	 *
-	 * @return  integer  The record ID on success, false on failure
-	 *
-	 * @since   4.0.0
-	 */
-	protected function save($recordKey = null)
-	{
-		$data = (array) json_decode($this->input->json->getRaw(), true);
+    /**
+     * Method to save a record.
+     *
+     * @param   integer  $recordKey  The primary key of the item (if exists)
+     *
+     * @return  integer  The record ID on success, false on failure
+     *
+     * @since   4.0.0
+     */
+    protected function save($recordKey = null)
+    {
+        $data = (array) json_decode($this->input->json->getRaw(), true);
 
-		foreach (FieldsHelper::getFields('com_weblinks.weblink') as $field)
-		{
-			if (isset($data[$field->name]))
-			{
-				!isset($data['com_fields']) && $data['com_fields'] = [];
+        foreach (FieldsHelper::getFields('com_weblinks.weblink') as $field) {
+            if (isset($data[$field->name])) {
+                !isset($data['com_fields']) && $data['com_fields'] = [];
 
-				$data['com_fields'][$field->name] = $data[$field->name];
-				unset($data[$field->name]);
-			}
-		}
+                $data['com_fields'][$field->name] = $data[$field->name];
+                unset($data[$field->name]);
+            }
+        }
 
-		$this->input->set('data', $data);
+        $this->input->set('data', $data);
 
-		return parent::save($recordKey);
-	}
+        return parent::save($recordKey);
+    }
 }
