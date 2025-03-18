818b989fca95cdfad94786e1e65d0f3b51784634
Nicola Galgano <optimus4joomla@gmail.com>
2025-03-18 10:04:29 +0100

    nullable_type_declaration_for_default_null_value

--- src/api/components/com_weblinks/View/Weblinks/JsonApiView.php
+++ src/api/components/com_weblinks/View/Weblinks/JsonApiView.php
@@ -98,7 +98,7 @@ class JsonApiView extends BaseApiView
      *
      * @since   4.0.0
      */
-    public function displayList(array $items = null)
+    public function displayList(?array $items = null)
     {
         foreach (FieldsHelper::getFields('com_weblinks.weblink') as $field) {
             $this->fieldsToRenderList[] = $field->name;
