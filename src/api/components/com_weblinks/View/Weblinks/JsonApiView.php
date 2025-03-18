47bc182ec3df255a17cc50a5c8ffacc32c2cd52d
Nicola Galgano <optimus4joomla@gmail.com>
2025-03-18 10:01:40 +0100

    code style

--- src/api/components/com_weblinks/View/Weblinks/JsonApiView.php
+++ src/api/components/com_weblinks/View/Weblinks/JsonApiView.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package     Joomla.API
  * @subpackage  com_weblinks
@@ -9,7 +10,7 @@
 
 namespace Joomla\Component\Weblinks\Api\View\Weblinks;
 
-defined('_JEXEC') or die;
+\defined('_JEXEC') or die;
 
 use Joomla\CMS\MVC\View\JsonApiView as BaseApiView;
 use Joomla\Component\Fields\Administrator\Helper\FieldsHelper;
@@ -21,127 +22,124 @@ use Joomla\Component\Fields\Administrator\Helper\FieldsHelper;
  */
 class JsonApiView extends BaseApiView
 {
-	/**
-	 * The fields to render item in the documents
-	 *
-	 * @var  array
-	 * @since  4.0.0
-	 */
-	protected $fieldsToRenderItem = [
-		'id',
-		'catid',
-		'title',
-		'alias',
-		'url',
-		'description',
-		'hits',
-		'state',
-		'checked_out',
-		'checked_out_time',
-		'ordering',
-		'access',
-		'params',
-		'language',
-		'created',
-		'created_by',
-		'created_by_alias',
-		'modified',
-		'modified_by',
-		'metakey',
-		'metadesc',
-		'metadata',
-		'featured',
-		'xreference',
-		'publish_up',
-		'publish_down',
-		'version',
-		'images',
-		'tags',
-	];
+    /**
+     * The fields to render item in the documents
+     *
+     * @var  array
+     * @since  4.0.0
+     */
+    protected $fieldsToRenderItem = [
+        'id',
+        'catid',
+        'title',
+        'alias',
+        'url',
+        'description',
+        'hits',
+        'state',
+        'checked_out',
+        'checked_out_time',
+        'ordering',
+        'access',
+        'params',
+        'language',
+        'created',
+        'created_by',
+        'created_by_alias',
+        'modified',
+        'modified_by',
+        'metakey',
+        'metadesc',
+        'metadata',
+        'featured',
+        'xreference',
+        'publish_up',
+        'publish_down',
+        'version',
+        'images',
+        'tags',
+    ];
 
-	/**
-	 * The fields to render items in the documents
-	 *
-	 * @var  array
-	 * @since  4.0.0
-	 */
-	protected $fieldsToRenderList = [
-		'id',
-		'title',
-		'alias',
-		'checked_out',
-		'checked_out_time',
-		'catid',
-		'created',
-		'created_by',
-		'hits',
-		'state',
-		'access',
-		'ordering',
-		'language',
-		'publish_up',
-		'publish_down',
-		'language_title',
-		'language_image',
-		'editor',
-		'access_level',
-		'category_title',
-	];
+    /**
+     * The fields to render items in the documents
+     *
+     * @var  array
+     * @since  4.0.0
+     */
+    protected $fieldsToRenderList = [
+        'id',
+        'title',
+        'alias',
+        'checked_out',
+        'checked_out_time',
+        'catid',
+        'created',
+        'created_by',
+        'hits',
+        'state',
+        'access',
+        'ordering',
+        'language',
+        'publish_up',
+        'publish_down',
+        'language_title',
+        'language_image',
+        'editor',
+        'access_level',
+        'category_title',
+    ];
 
-	/**
-	 * Execute and display a template script.
-	 *
-	 * @param   array|null  $items  Array of items
-	 *
-	 * @return  string
-	 *
-	 * @since   4.0.0
-	 */
-	public function displayList(array $items = null)
-	{
-		foreach (FieldsHelper::getFields('com_weblinks.weblink') as $field)
-		{
-			$this->fieldsToRenderList[] = $field->name;
-		}
+    /**
+     * Execute and display a template script.
+     *
+     * @param   array|null  $items  Array of items
+     *
+     * @return  string
+     *
+     * @since   4.0.0
+     */
+    public function displayList(array $items = null)
+    {
+        foreach (FieldsHelper::getFields('com_weblinks.weblink') as $field) {
+            $this->fieldsToRenderList[] = $field->name;
+        }
 
-		return parent::displayList();
-	}
+        return parent::displayList();
+    }
 
-	/**
-	 * Execute and display a template script.
-	 *
-	 * @param   object  $item  Item
-	 *
-	 * @return  string
-	 *
-	 * @since   4.0.0
-	 */
-	public function displayItem($item = null)
-	{
-		foreach (FieldsHelper::getFields('com_weblinks.weblink') as $field)
-		{
-			$this->fieldsToRenderItem[] = $field->name;
-		}
+    /**
+     * Execute and display a template script.
+     *
+     * @param   object  $item  Item
+     *
+     * @return  string
+     *
+     * @since   4.0.0
+     */
+    public function displayItem($item = null)
+    {
+        foreach (FieldsHelper::getFields('com_weblinks.weblink') as $field) {
+            $this->fieldsToRenderItem[] = $field->name;
+        }
 
-		return parent::displayItem();
-	}
+        return parent::displayItem();
+    }
 
-	/**
-	 * Prepare item before render.
-	 *
-	 * @param   object  $item  The model item
-	 *
-	 * @return  object
-	 *
-	 * @since   4.0.0
-	 */
-	protected function prepareItem($item)
-	{
-		foreach (FieldsHelper::getFields('com_weblinks.weblink', $item, true) as $field)
-		{
-			$item->{$field->name} = isset($field->apivalue) ? $field->apivalue : $field->rawvalue;
-		}
+    /**
+     * Prepare item before render.
+     *
+     * @param   object  $item  The model item
+     *
+     * @return  object
+     *
+     * @since   4.0.0
+     */
+    protected function prepareItem($item)
+    {
+        foreach (FieldsHelper::getFields('com_weblinks.weblink', $item, true) as $field) {
+            $item->{$field->name} = isset($field->apivalue) ? $field->apivalue : $field->rawvalue;
+        }
 
-		return parent::prepareItem($item);
-	}
+        return parent::prepareItem($item);
+    }
 }
