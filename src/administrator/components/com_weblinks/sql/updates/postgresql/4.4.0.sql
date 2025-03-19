7694b4c09b3e10104b8d9197e90c383878222eb9
Nicola Galgano <optimus4joomla@gmail.com>
2025-03-19 16:30:22 +0100

    Support Action Log
    Support basic action logs for weblinks

---
+++ src/administrator/components/com_weblinks/sql/updates/postgresql/4.4.0.sql
@@ -0,0 +1,4 @@
+INSERT INTO "#__action_logs_extensions" ("extension") VALUES ('com_weblinks');
+INSERT INTO "#__action_log_config" ("type_title", "type_alias", "id_holder", "title_holder", "table_name", "text_prefix") VALUES
+('weblink', 'com_weblinks.weblink', 'id', 'title', '#__weblinks', 'COM_WEBLINKS_ACTIONLOG'),
+('weblink', 'com_weblinks.form', 'id', 'title', '#__weblinks', 'COM_WEBLINKS_ACTIONLOG');
