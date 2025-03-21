263817f5d1bbd4b62589cfca3de17605a4a4bed8
Oleksandr <a.samoylovua@gmail.com>
2025-03-21 17:00:30 +0200

    Start webservices integration

---
+++ src/api/components/com_weblinks/Controller/WeblinksController.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @package     Joomla.API
+ * @subpackage  com_weblinks
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Component\Weblinks\Api\Controller;
+
+use Joomla\CMS\MVC\Controller\ApiController;
+use Joomla\Component\Fields\Administrator\Helper\FieldsHelper;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+/**
+ * The weblinks controller
+ *
+ * @since  4.0.0
+ */
+class WeblinksController extends ApiController
+{
+    /**
+     * The content type of the item.
+     *
+     * @var    string
+     * @since  4.0.0
+     */
+    protected $contentType = 'weblinks';
+
+    /**
+     * The default view for the display method.
+     *
+     * @var    string
+     * @since  3.0
+     */
+    protected $default_view = 'weblinks';
+
+    /**
+     * Method to save a record.
+     *
+     * @param   integer  $recordKey  The primary key of the item (if exists)
+     *
+     * @return  integer  The record ID on success, false on failure
+     *
+     * @since   4.0.0
+     */
+    protected function save($recordKey = null)
+    {
+        $data = (array) json_decode($this->input->json->getRaw(), true);
+
+        foreach (FieldsHelper::getFields('com_weblinks.weblink') as $field) {
+            if (isset($data[$field->name])) {
+                !isset($data['com_fields']) && $data['com_fields'] = [];
+
+                $data['com_fields'][$field->name] = $data[$field->name];
+                unset($data[$field->name]);
+            }
+        }
+
+        $this->input->set('data', $data);
+
+        return parent::save($recordKey);
+    }
+}
