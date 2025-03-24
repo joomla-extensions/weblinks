c2eccdbf95f9c417b69bc15b32bcfea29fcf105a
Amaan Ali <ali2003.amaan@gmail.com>
2025-03-24 12:32:05 +0530

    Add phpcbf support to runChecker in RoboFile and other files

---
+++ tests/unit/frontend/helper/WeblinksHelperRouteTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use Joomla\Component\Weblinks\Site\Helper\RouteHelper;
+use Joomla\Tests\Unit\UnitTestCase;
+
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_weblinks
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+class WeblinksHelperRouteTest extends UnitTestCase
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+        require_once 'src/components/com_weblinks/helpers/route.php';
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testGetFormRouteNewWeblink()
+    {
+        $this->assertStringContainsString('weblink.add&w_id=0', RouteHelper::getFormRoute(null));
+    }
+}
