1189409877371fd2dc79af6b91d9767a95ecba44
Amitesh007z <145351157+Amitesh007z@users.noreply.github.com>
2025-04-07 20:51:00 +0530

    fix styling

--- src/templates/cassiopeia/html/com_finder/search/default_result.php
+++ src/templates/cassiopeia/html/com_finder/search/default_result.php
@@ -3,7 +3,6 @@
 /**
  * @package     Joomla.Site
  * @subpackage  com_finder
- *
  * @copyright   (C) 2011 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
@@ -20,37 +19,34 @@ use Joomla\Component\Finder\Administrator\Indexer\Taxonomy;
 use Joomla\String\StringHelper;
 
 /** @var \Joomla\Component\Finder\Site\View\Search\HtmlView $this */
-$user             = $this->getCurrentUser();
+$user = $this->getCurrentUser();
 $show_description = $this->params->get('show_description', 1);
 
 if ($show_description) {
-    // Calculate number of characters to display around the result
     $term_length = StringHelper::strlen($this->query->input);
     $desc_length = $this->params->get('description_length', 255);
-    $pad_length  = $term_length < $desc_length ? (int) floor(($desc_length - $term_length) / 2) : 0;
+    $pad_length = $term_length < $desc_length ? (int) floor(($desc_length - $term_length) / 2) : 0;
 
-    // Make sure we highlight term both in introtext and fulltext
     $full_description = $this->result->description;
     if (!empty($this->result->summary) && !empty($this->result->body)) {
         $full_description = Helper::parse($this->result->summary . $this->result->body);
     }
 
-    // Find the position of the search term
-    $pos = $term_length ? StringHelper::strpos(StringHelper::strtolower($full_description), StringHelper::strtolower($this->query->input)) : false;
+    $pos = $term_length ? StringHelper::strpos(
+        StringHelper::strtolower($full_description),
+        StringHelper::strtolower($this->query->input)
+    ) : false;
 
-    // Find a potential start point
     $start = ($pos && $pos > $pad_length) ? $pos - $pad_length : 0;
-
-    // Find a space between $start and $pos, start right after it.
     $space = StringHelper::strpos($full_description, ' ', $start > 0 ? $start - 1 : 0);
     $start = ($space && $space < $pos) ? $space + 1 : $start;
 
     $description = HTMLHelper::_('string.truncate', StringHelper::substr($full_description, $start), $desc_length, true);
 }
 
-$showImage  = $this->params->get('show_image', 0);
+$showImage = $this->params->get('show_image', 0);
 $imageClass = $this->params->get('image_class', '');
-$extraAttr  = [];
+$extraAttr = [];
 
 if ($showImage && !empty($this->result->imageUrl) && $imageClass !== '') {
     $extraAttr['class'] = $imageClass;
@@ -61,12 +57,12 @@ if (!empty($this->result->mime)) {
     $icon = '<span class="icon-file-' . $this->result->mime . '" aria-hidden="true"></span> ';
 }
 
-
 $show_url = '';
 if ($this->params->get('show_url', 1)) {
     $show_url = '<cite class="result__title-url">' . $this->baseUrl . Route::_($this->result->cleanURL) . '</cite>';
 }
 ?>
+
 <li class="result__item">
     <?php if ($showImage && isset($this->result->imageUrl)) : ?>
         <figure class="<?php echo htmlspecialchars($imageClass, ENT_COMPAT, 'UTF-8'); ?> result__image">
@@ -79,35 +75,32 @@ if ($this->params->get('show_url', 1)) {
             <?php endif; ?>
         </figure>
     <?php endif; ?>
+
     <p class="result__title">
-    <?php if ($this->result->route) : ?>
-        <?php
+        <?php if ($this->result->route) : ?>
+            <?php
+            $url = Route::_($this->result->route);
 
+            if ($this->result->extension === 'com_weblinks' && !empty($this->result->id)) {
+                $itemid = isset($this->result->itemid) ? (int) $this->result->itemid : 0;
+                $url = Route::_('index.php?option=com_weblinks&task=weblink.go&id=' . (int) $this->result->id . '&Itemid=' . $itemid, false);
+            }
 
-        $url = Route::_($this->result->route);
-
-        if ($this->result->extension === 'com_weblinks' && !empty($this->result->id)) {
-            $itemid = isset($this->result->itemid) ? (int) $this->result->itemid : 0;
-            $url = Route::_('index.php?option=com_weblinks&task=weblink.go&id=' . (int) $this->result->id . '&Itemid=' . $itemid, false);
-        }
-
-        echo HTMLHelper::link(
-            $url,
-            '<span class="result__title-text">' . $icon . $this->result->title . '</span>' . $show_url,
-            [
-            'class' => 'result__title-link',
-            'target' => '_blank',
-            'rel' => 'noopener noreferrer'
-            ]
-        );
-
-
-        ?>
-    <?php else : ?>
-        <?php echo $this->result->title; ?>
-    <?php endif; ?>
-
+            echo HTMLHelper::link(
+                $url,
+                '<span class="result__title-text">' . $icon . $this->result->title . '</span>' . $show_url,
+                [
+                    'class' => 'result__title-link',
+                    'target' => '_blank',
+                    'rel' => 'noopener noreferrer'
+                ]
+            );
+            ?>
+        <?php else : ?>
+            <?php echo $this->result->title; ?>
+        <?php endif; ?>
     </p>
+
     <?php if ($show_description && $description !== '') : ?>
         <p class="result__description">
             <?php if ($this->result->start_date && $this->params->get('show_date', 1)) : ?>
@@ -118,6 +111,7 @@ if ($this->params->get('show_url', 1)) {
             <?php echo $description; ?>
         </p>
     <?php endif; ?>
+
     <?php $taxonomies = $this->result->getTaxonomy(); ?>
     <?php if (count($taxonomies) && $this->params->get('show_taxonomy', 1)) : ?>
         <ul class="result__taxonomy">
@@ -125,6 +119,7 @@ if ($this->params->get('show_url', 1)) {
                 <?php if ($type == 'Language' && (!Multilanguage::isEnabled() || (isset($taxonomy[0]) && $taxonomy[0]->title == '*'))) : ?>
                     <?php continue; ?>
                 <?php endif; ?>
+
                 <?php $branch = Taxonomy::getBranch($type); ?>
                 <?php if ($branch->state == 1 && in_array($branch->access, $user->getAuthorisedViewLevels())) : ?>
                     <?php $taxonomy_text = []; ?>
@@ -133,6 +128,7 @@ if ($this->params->get('show_url', 1)) {
                             <?php $taxonomy_text[] = $node->title; ?>
                         <?php endif; ?>
                     <?php endforeach; ?>
+
                     <?php if (count($taxonomy_text)) : ?>
                         <li class="result__taxonomy-item result__taxonomy--<?php echo $type; ?>">
                             <span><?php echo Text::_(LanguageHelper::branchSingular($type)); ?>:</span>
