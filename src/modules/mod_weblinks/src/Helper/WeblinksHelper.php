e55c34f986045392bf334e8259cd0181732e658a
Your Name <your-email@example.com>
2025-04-18 17:26:24 +0530

    made sure it works ,when it deep nested

--- src/modules/mod_weblinks/src/Helper/WeblinksHelper.php
+++ src/modules/mod_weblinks/src/Helper/WeblinksHelper.php
@@ -1,144 +1,88 @@
 <?php
 
-/**
- * @package     Joomla.Administrator
- * @subpackage  Weblinks
- *
- * @copyright   Copyright (C) 2005 - 2025 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
 namespace Joomla\Module\Weblinks\Site\Helper;
 
-// phpcs:ignoreFile -- allow _JEXEC check for Joomla module security
 \defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory as JoomlaFactory;
 use Joomla\CMS\Application\CMSApplicationInterface;
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Router\Route;
 use Joomla\Registry\Registry;
 
-/**
- * Helper for mod_weblinks
- *
- * @since  1.5
- */
 class WeblinksHelper
 {
-    /**
-     * Retrieve list of weblinks
-     *
-     * @param   Registry                 $params  The module parameters
-     * @param   CMSApplicationInterface  $app     The application
-     *
-     * @return  array   Array containing all weblinks, including a separate entry for parent category weblinks
-     *
-     * @since   1.5
-     */
     public function getWeblinks($params, $app)
     {
-        $parentModel = $app->bootComponent('com_weblinks')->getMVCFactory()
-            ->createModel('Category', 'Site', ['ignore_request' => true]);
-
-        $cParams = ComponentHelper::getParams('com_weblinks');
-        $parentModel->setState('params', $cParams);
-        $parentModel->setState('list.start', 0);
-        $parentModel->setState('list.limit', (int) $params->get('count', 5));
-        $parentModel->setState('filter.state', 1);
-        $parentModel->setState('filter.publish_date', true);
-        $parentModel->setState('filter.access', !ComponentHelper::getParams('com_weblinks')->get('show_noauth'));
-        $parentModel->setState('list.ordering', $params->get('ordering', 'ordering') == 'order' ? 'ordering' : $params->get('ordering'));
-        $parentModel->setState('list.direction', $params->get('direction', 'asc'));
-        $parentModel->setState('filter.language', $app->getLanguageFilter());
-        $parentModel->setState('filter.c.published', 1);
-
+        $db = JoomlaFactory::getDbo();
+        $catid = (int) $params->get('catid', 0);
         $parentWeblinks = [];
-        $catid          = (int) $params->get('catid', 0);
-        if ($catid) {
-            $parentModel->setState('category.id', $catid);
-            $parentModel->setState('category.group', 0);
-            $case_when1 = ' CASE WHEN ' . $parentModel->getDbo()->getQuery(true)->charLength('a.alias', '!=', '0') .
-                         ' THEN ' . $parentModel->getDbo()->getQuery(true)->concatenate([$parentModel->getDbo()->getQuery(true)->castAs('CHAR', 'a.id'), 'a.alias'], ':') .
-                         ' ELSE ' . $parentModel->getDbo()->getQuery(true)->castAs('CHAR', 'a.id') . ' END as slug';
-            $case_when2 = ' CASE WHEN ' . $parentModel->getDbo()->getQuery(true)->charLength('c.alias', '!=', '0') .
-                         ' THEN ' . $parentModel->getDbo()->getQuery(true)->concatenate([$parentModel->getDbo()->getQuery(true)->castAs('CHAR', 'c.id'), 'c.alias'], ':') .
-                         ' ELSE ' . $parentModel->getDbo()->getQuery(true)->castAs('CHAR', 'c.id') . ' END as catslug';
-            $parentModel->setState('list.select', 'a.*, c.description AS c_description, c.published AS c_published,' . $case_when1 . ',' . $case_when2);
-            $parentItems = $parentModel->getItems();
-            if ($parentItems) {
-                foreach ($parentItems as $item) {
-                    $temp         = $item->params;
-                    $item->params = clone $cParams;
-                    $item->params->merge($temp);
-                    $item->link = $item->params->get('count_clicks', 1) == 1
-                        ? Route::_('index.php?option=com_weblinks&task=weblink.go&catid=' . $item->catslug . '&id=' . $item->slug)
-                        : $item->url;
-                    $parentWeblinks[] = $item;
+        $allWeblinks = [];
+
+        if ($catid > 0) {
+            $cParams = ComponentHelper::getParams('com_weblinks');
+
+            $categoryQuery = $db->getQuery(true)
+                ->select('lft, rgt')
+                ->from('#__categories')
+                ->where('id = ' . (int) $catid)
+                ->where('extension = ' . $db->quote('com_weblinks'))
+                ->where('published = 1');
+            $db->setQuery($categoryQuery);
+            $rootCategory = $db->loadObject();
+
+            if ($rootCategory) {
+                $categoryIdsQuery = $db->getQuery(true)
+                    ->select('id')
+                    ->from('#__categories')
+                    ->where('extension = ' . $db->quote('com_weblinks'))
+                    ->where('published = 1')
+                    ->where('lft >= ' . (int) $rootCategory->lft)
+                    ->where('rgt <= ' . (int) $rootCategory->rgt);
+                $db->setQuery($categoryIdsQuery);
+                $categoryIds = $db->loadColumn();
+
+                if (!empty($categoryIds)) {
+                    $weblinksQuery = $db->getQuery(true)
+                        ->select('a.*, c.title AS category_title')
+                        ->from('#__weblinks AS a')
+                        ->join('LEFT', '#__categories AS c ON a.catid = c.id')
+                        ->where('a.state = 1')
+                        ->where('a.catid IN (' . implode(',', $categoryIds) . ')');
+
+                    if ($app->getLanguageFilter()) {
+                        $weblinksQuery->where('a.language IN (' . $db->quote($app->getLanguage()->getTag()) . ',' . $db->quote('*') . ')');
+                    }
+
+                    $weblinksQuery->order($params->get('ordering', 'ordering') . ' ' . $params->get('direction', 'ASC'));
+                    $db->setQuery($weblinksQuery);
+                    $items = $db->loadObjectList();
+
+                    foreach ($items as $item) {
+                        $item->params = clone $cParams;
+                        $item->slug = $item->alias ? ($item->id . ':' . $item->alias) : $item->id;
+                        $item->catslug = $item->catid . ':' . (isset($item->category_alias) ? $item->category_alias : $item->category_title);
+                        $item->link = $item->params->get('count_clicks', 1) == 1
+                            ? Route::_('index.php?option=com_weblinks&task=weblink.go&catid=' . $item->catslug . '&id=' . $item->slug)
+                            : $item->url;
+
+                        $allWeblinks[] = $item;
+                        if ($item->catid == $catid) {
+                            $parentWeblinks[] = $item;
+                        }
+                    }
                 }
             }
         }
 
-        $categoryModel = $app->bootComponent('com_weblinks')->getMVCFactory()
-            ->createModel('Category', 'Site', ['ignore_request' => true]);
-        $categoryModel->setState('params', $cParams);
-        $categoryModel->setState('list.start', 0);
-        $categoryModel->setState('list.limit', (int) $params->get('count', 5));
-        $categoryModel->setState('filter.state', 1);
-        $categoryModel->setState('filter.publish_date', true);
-        $categoryModel->setState('filter.access', !ComponentHelper::getParams('com_weblinks')->get('show_noauth'));
-        $categoryModel->setState('list.ordering', $params->get('ordering', 'ordering') == 'order' ? 'ordering' : $params->get('ordering'));
-        $categoryModel->setState('list.direction', $params->get('direction', 'asc'));
-        $categoryModel->setState('filter.language', $app->getLanguageFilter());
-        $categoryModel->setState('filter.c.published', 1);
-
-        if ($catid) {
-            $categoryModel->setState('category.id', $catid);
-            $categoryModel->setState('category.group', $params->get('groupby', 0));
-            $categoryModel->setState('category.ordering', $params->get('groupby_ordering', 'c.lft'));
-            $categoryModel->setState('category.direction', $params->get('groupby_direction', 'ASC'));
-            $case_when1 = ' CASE WHEN ' . $categoryModel->getDbo()->getQuery(true)->charLength('a.alias', '!=', '0') .
-                         ' THEN ' . $categoryModel->getDbo()->getQuery(true)->concatenate([$categoryModel->getDbo()->getQuery(true)->castAs('CHAR', 'a.id'), 'a.alias'], ':') .
-                         ' ELSE ' . $categoryModel->getDbo()->getQuery(true)->castAs('CHAR', 'a.id') . ' END as slug';
-            $case_when2 = ' CASE WHEN ' . $categoryModel->getDbo()->getQuery(true)->charLength('c.alias', '!=', '0') .
-                         ' THEN ' . $categoryModel->getDbo()->getQuery(true)->concatenate([$categoryModel->getDbo()->getQuery(true)->castAs('CHAR', 'c.id'), 'c.alias'], ':') .
-                         ' ELSE ' . $categoryModel->getDbo()->getQuery(true)->castAs('CHAR', 'c.id') . ' END as catslug';
-            $categoryModel->setState('list.select', 'a.*, c.description AS c_description, c.published AS c_published,' . $case_when1 . ',' . $case_when2);
-            $items = $categoryModel->getItems();
-        } else {
-            $items = [];
-        }
-
-        if ($items) {
-            foreach ($items as $item) {
-                $temp         = $item->params;
-                $item->params = clone $cParams;
-                $item->params->merge($temp);
-                $item->link = $item->params->get('count_clicks', 1) == 1
-                    ? Route::_('index.php?option=com_weblinks&task=weblink.go&catid=' . $item->catslug . '&id=' . $item->slug)
-                    : $item->url;
-            }
-        } else {
-            $items = [];
-        }
+        file_put_contents('debug.log', "Fetched Weblinks: " . print_r($allWeblinks, true) . "\n", FILE_APPEND);
 
         return [
-            'parentWeblinks'   => $parentWeblinks,
-            'categoryWeblinks' => $items,
+            'parentWeblinks' => $parentWeblinks,
+            'categoryWeblinks' => $allWeblinks,
         ];
     }
 
-    /**
-     * Retrieve list of weblinks
-     *
-     * @param   Registry                 $params  The module parameters
-     * @param   CMSApplicationInterface  $app     The application
-     *
-     * @return  mixed   Null if no weblinks based on input parameters else an array containing all the weblinks.
-     *
-     * @since   1.5
-     *
-     * @deprecated 5.0 Use the none static function getWeblinks
-     */
     public static function getList($params, $app)
     {
         return (new self())->getWeblinks($params, $app);
--- src/modules/mod_weblinks/tmpl/default.php
+++ src/modules/mod_weblinks/tmpl/default.php
@@ -1,29 +1,19 @@
 <?php
 
-/**
- * @package    Joomla.Site
- * @subpackage mod_weblinks
- */
-
 use Joomla\CMS\Factory as JoomlaFactory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
-// phpcs:ignoreFile -- allow _JEXEC check for Joomla module security
 \defined('_JEXEC') or die;
-
 
 HTMLHelper::_('bootstrap.framework');
 HTMLHelper::_('bootstrap.modal');
 $db = JoomlaFactory::getDbo();
 
-
-if (!\function_exists('getCategoryTree')) {
+if (!function_exists('getCategoryTree')) {
     function getCategoryTree($parentId, $db)
     {
         $tree = [];
-
-
         $query = $db->getQuery(true)
             ->select('id, title, parent_id')
             ->from('#__categories')
@@ -37,7 +27,6 @@ if (!\function_exists('getCategoryTree')) {
             return [];
         }
 
-
         $node = [
             'catid'     => $parent->id,
             'title'     => $parent->title,
@@ -45,45 +34,55 @@ if (!\function_exists('getCategoryTree')) {
             'children'  => [],
         ];
 
-
         $query = $db->getQuery(true)
             ->select('id')
             ->from('#__categories')
             ->where('parent_id = ' . (int) $parentId)
             ->where('extension = ' . $db->quote('com_weblinks'))
-            ->where('published = 1');
+            ->where('published = 1')
+            ->order('lft ASC');
         $db->setQuery($query);
         $children = $db->loadColumn();
 
         foreach ($children as $childId) {
-            $node['children'][] = getCategoryTree($childId, $db);
+            $childNode = getCategoryTree($childId, $db);
+            if (!empty($childNode)) {
+                $node['children'][] = $childNode;
+            }
         }
 
         return $node;
     }
 }
 
-if (!\function_exists('renderCategoryNode')) {
-    function renderCategoryNode($node, $list, $params, $moduleclass_sfx, $parentWeblinks = [])
+if (!function_exists('renderCategoryNode')) {
+    function renderCategoryNode($node, $list, $params, $moduleclass_sfx, $parentWeblinks = [], $db, $isRoot = false)
     {
         if (!$node || !isset($node['catid'])) {
             return;
         }
 
-        $catid    = $node['catid'];
-        $items    = array_filter($list, fn ($item) => (int) $item->catid === (int) $catid);
+        $catid = $node['catid'];
+        $items = getWeblinksForNode($node, $list['categoryWeblinks']);
+
+        if ($isRoot) {
+            $uniqueParentItems = array_udiff($parentWeblinks, $items, fn($a, $b) => $a->id - $b->id);
+            $items = array_merge($items, $uniqueParentItems);
+        }
+
         $hasItems = !empty($items);
 
-        echo '<div class="category-block">';
+        echo '<div class="category-block" style="margin-left: ' . (20 * ($node['parent_id'] > 1 ? count(explode(',', $node['parent_id'])) : 0)) . 'px;">';
 
         if ($params->get('groupby_showtitle', 1)) {
             echo '<h4 class="weblink-category-title">' . htmlspecialchars($node['title'], ENT_COMPAT, 'UTF-8') . '</h4>';
         }
 
-
-        if ($node['catid'] == (int) $params->get('catid') && !empty($parentWeblinks)) {
-            echo '<ul class="weblinks' . $moduleclass_sfx . '">';
-            foreach ($parentWeblinks as $item) {
+        echo '<ul class="weblinks' . $moduleclass_sfx . '">';
+        if (!$hasItems) {
+            echo '<li><em>' . Text::_('MOD_WEBLINKS_NO_ITEMS') . '</em></li>';
+        } else {
+            foreach ($items as $item) {
                 $link   = $item->link;
                 $width  = (int) $item->params->get('width', 600);
                 $height = (int) $item->params->get('height', 500);
@@ -93,29 +92,29 @@ if (!\function_exists('renderCategoryNode')) {
                 switch ($item->params->get('target')) {
                     case 1:
                         echo '<a href="' . $link . '" target="_blank" rel="' . $params->get('follow', 'nofollow') . '">' .
-                        htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
+                             htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
                         break;
                     case 2:
                         $attribs = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=' . $width . ',height=' . $height;
                         echo "<a href=\"$link\" onclick=\"window.open(this.href, 'targetWindow', '$attribs'); return false;\">" .
-                        htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
+                             htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
                         break;
                     case 3:
                         $modalId     = 'weblink-item-modal-' . $item->id;
                         $modalParams = [
-                        'title'      => htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8'),
-                        'url'        => $link,
-                        'height'     => '100%',
-                        'width'      => '100%',
-                        'bodyHeight' => 70,
-                        'modalWidth' => 80,
+                            'title'      => htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8'),
+                            'url'        => $link,
+                            'height'     => '100%',
+                            'width'      => '100%',
+                            'bodyHeight' => 70,
+                            'modalWidth' => 80,
                         ];
                         echo HTMLHelper::_('bootstrap.renderModal', $modalId, $modalParams);
                         echo '<button type="button" class="btn btn-link" data-bs-toggle="modal" data-bs-target="#' . $modalId . '">' . $item->title . '</button>';
                         break;
                     default:
                         echo '<a href="' . $link . '" rel="' . $params->get('follow', 'nofollow') . '">' .
-                        htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
+                             htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
                 }
 
                 echo '</div>';
@@ -126,85 +125,35 @@ if (!\function_exists('renderCategoryNode')) {
 
                 if ($params->get('hits', 0)) {
                     echo '<div class="col flex-sm-grow-1"><span class="badge bg-info float-md-end">' .
-                        $item->hits . ' ' . Text::_('MOD_WEBLINKS_HITS') . '</span></div>';
+                         $item->hits . ' ' . Text::_('MOD_WEBLINKS_HITS') . '</span></div>';
                 }
 
                 echo '</div></li>';
             }
-            echo '</ul>';
         }
-
-
-        echo '<ul class="weblinks' . $moduleclass_sfx . '">';
-        if (!$hasItems && ($node['catid'] != (int) $params->get('catid') || empty($parentWeblinks))) {
-            echo '<li><em>' . Text::_('MOD_WEBLINKS_NO_ITEMS') . '</em></li>';
-        }
-
-        foreach ($items as $item) {
-            $link   = $item->link;
-            $width  = (int) $item->params->get('width', 600);
-            $height = (int) $item->params->get('height', 500);
-
-            echo '<li><div class="d-flex flex-wrap"><div class="col flex-sm-grow-1">';
-
-            switch ($item->params->get('target')) {
-                case 1:
-                    echo '<a href="' . $link . '" target="_blank" rel="' . $params->get('follow', 'nofollow') . '">' .
-                    htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
-                    break;
-                case 2:
-                    $attribs = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=' . $width . ',height=' . $height;
-                    echo "<a href=\"$link\" onclick=\"window.open(this.href, 'targetWindow', '$attribs'); return false;\">" .
-                    htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
-                    break;
-                case 3:
-                    $modalId     = 'weblink-item-modal-' . $item->id;
-                    $modalParams = [
-                    'title'      => htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8'),
-                    'url'        => $link,
-                    'height'     => '100%',
-                    'width'      => '100%',
-                    'bodyHeight' => 70,
-                    'modalWidth' => 80,
-                    ];
-                    echo HTMLHelper::_('bootstrap.renderModal', $modalId, $modalParams);
-                    echo '<button type="button" class="btn btn-link" data-bs-toggle="modal" data-bs-target="#' . $modalId . '">' . $item->title . '</button>';
-                    break;
-                default:
-                    echo '<a href="' . $link . '" rel="' . $params->get('follow', 'nofollow') . '">' .
-                    htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
-            }
-
-            echo '</div>';
-
-            if ($params->get('description', 0)) {
-                echo '<div class="col flex-sm-grow-1">' . $item->description . '</div>';
-            }
-
-            if ($params->get('hits', 0)) {
-                echo '<div class="col flex-sm-grow-1"><span class="badge bg-info float-md-end">' .
-                    $item->hits . ' ' . Text::_('MOD_WEBLINKS_HITS') . '</span></div>';
-            }
-
-            echo '</div></li>';
-        }
-
         echo '</ul>';
 
-
         foreach ($node['children'] as $childNode) {
-            renderCategoryNode($childNode, $list, $params, $moduleclass_sfx, $parentWeblinks);
+            renderCategoryNode($childNode, $list, $params, $moduleclass_sfx, [], $db, false);
         }
 
         echo '</div>';
     }
 }
 
+function getWeblinksForNode($node, $weblinks)
+{
+    $items = array_filter($weblinks, function ($item) use ($node) {
+        return (int) $item->catid === (int) $node['catid'];
+    });
+
+    return array_values($items);
+}
 
 if ($params->get('groupby', 0)) {
     $rootCatId    = (int) $params->get('catid');
     $categoryTree = getCategoryTree($rootCatId, $db);
-    renderCategoryNode($categoryTree, $list['categoryWeblinks'], $params, $moduleclass_sfx, $list['parentWeblinks']);
+    renderCategoryNode($categoryTree, $list, $params, $moduleclass_sfx, $list['parentWeblinks'], $db, true);
 } else {
     ?>
     <ul class="weblinks<?php echo $moduleclass_sfx; ?>">
@@ -219,29 +168,29 @@ if ($params->get('groupby', 0)) {
                         <?php switch ($item->params->get('target')) :
                             case 1:
                                 echo '<a href="' . $link . '" target="_blank" rel="' . $params->get('follow', 'nofollow') . '">' .
-                                htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
+                                     htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
                                 break;
                             case 2:
                                 $attribs = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=' . $width . ',height=' . $height;
                                 echo "<a href=\"$link\" onclick=\"window.open(this.href, 'targetWindow', '$attribs'); return false;\">" .
-                                htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
+                                     htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
                                 break;
                             case 3:
                                 $modalId     = 'weblink-item-modal-' . $item->id;
                                 $modalParams = [
-                                'title'      => htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8'),
-                                'url'        => $link,
-                                'height'     => '100%',
-                                'width'      => '100%',
-                                'bodyHeight' => 70,
-                                'modalWidth' => 80,
+                                    'title'      => htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8'),
+                                    'url'        => $link,
+                                    'height'     => '100%',
+                                    'width'      => '100%',
+                                    'bodyHeight' => 70,
+                                    'modalWidth' => 80,
                                 ];
                                 echo HTMLHelper::_('bootstrap.renderModal', $modalId, $modalParams);
                                 echo '<button type="button" class="btn btn-link" data-bs-toggle="modal" data-bs-target="#' . $modalId . '">' . $item->title . '</button>';
                                 break;
                             default:
                                 echo '<a href="' . $link . '" rel="' . $params->get('follow', 'nofollow') . '">' .
-                                htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
+                                     htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
                         endswitch; ?>
                     </div>
 
@@ -253,12 +202,10 @@ if ($params->get('groupby', 0)) {
                         <div class="col flex-sm-grow-1">
                             <span class="badge bg-info float-md-end"><?php echo $item->hits . ' ' . Text::_('MOD_WEBLINKS_HITS'); ?></span>
                         </div>
-                        
                     <?php endif; ?>
                 </div>
             </li>
         <?php endforeach; ?>
-        
     </ul>
     <?php
 }
