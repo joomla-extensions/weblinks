737d16de8adb179f00a7c7ffa34a05775c4edd9f
mahmoudmagdy1-1 <d4rkppl1@gmail.com>
2025-06-18 10:50:42 +0300

    Webservices Cypress tests to cover the rest of the endpoints

---
+++ tests/cypress/integration/api/com_weblinks/Categories.cy.js
@@ -0,0 +1,67 @@
+describe('Test that weblinks categories API endpoint', () => {
+  afterEach(() => cy.task('queryDB', "DELETE FROM #__categories WHERE title LIKE '%automated test category%' AND extension = 'com_weblinks'"));
+
+  it('can deliver a list of weblink categories', () => {
+    cy.db_enableExtension('1', 'plg_webservices_weblinks');
+    cy.db_createCategory({ title: 'automated test category', extension: 'com_weblinks' })
+      .then(() => cy.api_get('/weblinks/categories'))
+      .then((response) => cy.wrap(response).its('body').its('data.0').its('attributes')
+        .its('title')
+        .should('include', 'automated test category'));
+    cy.db_enableExtension('0', 'plg_webservices_weblinks');
+  });
+
+  it('can deliver a single weblink category', () => {
+    cy.db_enableExtension('1', 'plg_webservices_weblinks');
+    cy.db_createCategory({ title: 'automated test category', extension: 'com_weblinks' })
+      .then((categoryId) => cy.api_get(`/weblinks/categories/${categoryId}`))
+      .then((response) => cy.wrap(response).its('body').its('data').its('attributes')
+        .its('title')
+        .should('include', 'automated test category'));
+    cy.db_enableExtension('0', 'plg_webservices_weblinks');
+  });
+
+  it('can create a weblink category', () => {
+    cy.db_enableExtension('1', 'plg_webservices_weblinks');
+    cy.api_post('/weblinks/categories', {
+      title: 'automated test category',
+      alias: 'automated-test-category',
+      extension: 'com_weblinks',
+      published: 1,
+      access: 1,
+      language: '*',
+    })
+      .then((response) => cy.wrap(response).its('body').its('data').its('attributes')
+        .its('title')
+        .should('include', 'automated test category'));
+    cy.db_enableExtension('0', 'plg_webservices_weblinks');
+  });
+
+  it('can update a weblink category', () => {
+    cy.db_enableExtension('1', 'plg_webservices_weblinks');
+    cy.db_createCategory({ title: 'automated test category', extension: 'com_weblinks' })
+      .then((categoryId) => cy.api_patch(`/weblinks/categories/${categoryId}`, { title: 'updated automated test category' }))
+      .then((response) => cy.wrap(response).its('body').its('data').its('attributes')
+        .its('title')
+        .should('include', 'updated automated test category'));
+    cy.db_enableExtension('0', 'plg_webservices_weblinks');
+  });
+
+  it('can delete a weblink category', () => {
+    cy.db_enableExtension('1', 'plg_webservices_weblinks');
+    cy.api_post('/weblinks/categories', {
+      title: 'automated test category',
+      alias: 'automated-test-category',
+      extension: 'com_weblinks',
+      published: -2,
+      access: 1,
+      language: '*',
+    })
+      .then((response) => response.body.data.id)
+      .then((categoryId) => cy.api_delete(`/weblinks/categories/${categoryId}`))
+      .then((response) => {
+        expect(response.status).to.eq(204);
+      });
+    cy.db_enableExtension('0', 'plg_webservices_weblinks');
+  });
+});
