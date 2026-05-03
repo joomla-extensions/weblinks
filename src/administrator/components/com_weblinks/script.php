ef07d98611008caf0c0a4ff75bd65847d3135206
Nicola Galgano <optimus4joomla@gmail.com>
2026-05-03 08:16:03 +0000

    istall-language

--- src/administrator/components/com_weblinks/script.php
+++ src/administrator/components/com_weblinks/script.php
@@ -44,6 +44,7 @@ return new class () implements ServiceProviderInterface {
                 {
                     $this->app = $app;
                     $this->db  = $db;
+                    $this->app ->getLanguage()->load('com_patchtester.sys', JPATH_ADMINISTRATOR, null, true);
                 }
 
                 public function install(InstallerAdapter $parent): bool
