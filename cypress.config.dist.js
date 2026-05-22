8623c6005be48a0c50584292cdbe9aa924f6e058
Nicola Galgano <optimus4joomla@gmail.com>
2026-05-22 10:25:15 +0200

    Refactor Cypress config to consolidate env variables
    Moved expose configuration to env section.

--- cypress.config.dist.js
+++ cypress.config.dist.js
@@ -27,11 +27,9 @@ export default defineConfig({
     allowCypressEnv: false,
   },
   expose: {
-      sitename: 'Joomla CMS Test',
-      name: 'jane doe',
-      username: 'ci-admin',
-    },
-  env: {
+    sitename: 'Joomla CMS Test',
+    name: 'jane doe',
+    username: 'ci-admin',
     password: 'joomla-17082005',
     db_type: process.env.DB_TYPE || 'MySQLi',
     db_host: process.env.DB_HOST || 'mysql',
