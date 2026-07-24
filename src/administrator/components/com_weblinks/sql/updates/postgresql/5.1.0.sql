cd199dec30e18a79b1fb6c320326e381f772d2bf
Nicola Galgano <optimus4joomla@gmail.com>
2026-07-24 09:45:38 +0200

    Refactor JSONB updates for content types

--- src/administrator/components/com_weblinks/sql/updates/postgresql/5.1.0.sql
+++ src/administrator/components/com_weblinks/sql/updates/postgresql/5.1.0.sql
@@ -1,15 +1,19 @@
 UPDATE "#__content_types" 
 SET "table" = jsonb_set(
-    jsonb_set("table"::jsonb, '{special,type}', '"WeblinkTable"'),
-    '{special,prefix}', '"Joomla\\Component\\Weblinks\\Administrator\\Table\\"',
+    jsonb_set(
+        jsonb_set("table"::jsonb, '{special,type}', '"WeblinkTable"'),
+        '{special,prefix}', '"Joomla\\Component\\Weblinks\\Administrator\\Table\\"'
+    ),
     '{dbtable,prefix}', '"Joomla\\CMS\\Table\\"'
 )
 WHERE "type_alias" = 'com_weblinks.weblink';
 
 UPDATE "#__content_types" 
 SET "table" = jsonb_set(
-    jsonb_set("table"::jsonb, '{special,type}', '"CategoryTable"'),
-    '{special,prefix}', '"Joomla\\Component\\Categories\\Administrator\\Table\\"',
+    jsonb_set(
+        jsonb_set("table"::jsonb, '{special,type}', '"CategoryTable"'),
+        '{special,prefix}', '"Joomla\\Component\\Categories\\Administrator\\Table\\"'
+    ),
     '{dbtable,prefix}', '"Joomla\\CMS\\Table\\"'
 )
 WHERE "type_alias" = 'com_weblinks.category';
